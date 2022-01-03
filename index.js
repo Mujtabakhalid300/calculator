4b75003c6c797ecac584d7c619714545aa272312
Mujtaba <mujtabakhalid20@gmail.com>
2022-01-03 11:21:47 +0500

    multiple arguments support

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+var evaluateString = ""
 var btn = Array.from(document.getElementsByClassName('btn'))
 var display = document.getElementById('display')
 var num1
 var num2
 var operator
+var plus = "+"
+var minus = "-"
+var divide = "/"
+var multiply = "*"
 
 // console.log(typeof(parseInt('10')))
 
@@ -32,12 +37,14 @@ function checker(element){
     {element.addEventListener('click', function(){
         if(element.innerText=='Clear'){
             display.innerText = ""
+            evaluateString = ""
             operator = undefined
 
         }
         else if(element.innerText=='+'){
             display.innerText += " ".concat(element.innerText)
             display.innerText += "\xa0"
+            evaluateString += plus
 
             num1 = display.innerText.split('+')
             if(num1[0].includes('.')){
@@ -52,6 +59,9 @@ function checker(element){
         else if(element.innerText=='-'){
             display.innerText += " ".concat(element.innerText)
             display.innerText += "\xa0"
+            evaluateString += minus
+
+
 
             num1 = display.innerText.split('-')
             if(num1[0].includes('.')){
@@ -66,6 +76,9 @@ function checker(element){
         else if(element.innerText=='×'){
             display.innerText += " ".concat(element.innerText)
             display.innerText += "\xa0"
+            evaluateString += multiply
+
+
 
             num1 = display.innerText.split('×')
             if(num1[0].includes('.')){
@@ -80,6 +93,9 @@ function checker(element){
         else if(element.innerText=='÷'){
             display.innerText += " ".concat(element.innerText)
             display.innerText += "\xa0"
+            evaluateString += divide
+
+
 
             num1 = display.innerText.split('÷')
             if(num1[0].includes('.')){
@@ -94,6 +110,7 @@ function checker(element){
         else if(element.innerText=="="){
             num2 = display.innerText
             num2 = num2.split(operator)
+            try{
             if(num2[1].includes('.')){
                 num2 = parseFloat(num2[1])
             }
@@ -101,11 +118,24 @@ function checker(element){
                 num2 = parseInt(num2[1])
 
             }
-            display.innerText = work(operator)
-            
         }
+        catch(err){
+            alert('Invalid Argument Given')
+            display.innerText = ""
+        }   
+        if(eval(evaluateString)==undefined){
+                display.innerText=""
+        }else{
+            display.innerText = eval(evaluateString)
+        }
+        
+            }
+            
+        
         else{
             display.innerText = display.innerText.concat(element.innerText)
+            evaluateString += element.innerText
+
 
         }
     })}
